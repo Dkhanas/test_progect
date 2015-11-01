54e9fc2cf2d49cd29e103753cd8eb2f5f9208d21
Dmytro Khanas <dkhanas@gmail.com>
2015-11-01 18:55:23 +0200

    was created project and was made testapp

---
+++ test_project/testapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from testapp.models import ForTest
+
+admin.site.register(ForTest)
---
+++ test_project/testapp/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class ForTest(models.Model):
+    name = models.CharField(max_length="15")
+    prosessed = models.BooleanField(default=False)
